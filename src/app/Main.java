74438b71ad8599c9af14baa2938ad553067f5697
Puffl3x <limbs95@gmail.com>
2023-04-30 22:13:47 +0300

    Add multiply

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,11 +2,13 @@ package app;
 
 public class Main {
 
+    private static int a = 8;
+    private static int b = 2;
+
     public static void main(String[] args) {
         System.out.println("Calculator for base calculations");
         System.out.println("Version 1.0");
-        int a = 8;
-        int b = 2;
+
         int c = add(a, b);
         System.out.println(c);
         int d = multiply(a, b);
